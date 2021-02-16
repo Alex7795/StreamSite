bc3716e12f105a646b8ffbfd92d0ceafa545ef5c
mjni.skp <mjni.skp@edu.mercantec.dk>
2021-02-16 12:50:46 +0100

    first commit

---
+++ Streaming.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="MinLIste.php">Min Liste</a>
+  <a href="FortsætMedAtSe.php">Fortsæt med at se</a>
+
+  <div class="dropdown">
+    <button class="dropbtn">Film
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="Action.php">Action</a>
+      <a href="Gyser.php">Gyser</a>
+      <a href="Komedie.php">Komedie</a>
+    </div>
+  </div> 
+  <div class="dropdown">
+    <button class="dropbtn">Serier 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="Action.php">Action</a>
+      <a href="Gyser.php">Eventyr</a>
+      <a href="Komedie.php">Komedie</a>
+    </div>
+  </div> 
+
+  <input type="text" placeholder="Search..">
+</div>
+
+</body>
