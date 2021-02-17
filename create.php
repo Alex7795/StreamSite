291768723525958badbba5e46acb7dc013c26a51
Melika Emma Vigh Kjærgaard Pedersen <meli0970@edu.mercantec.dk>
2021-02-17 14:33:24 +0100

    tilføjer login samt gemme brugernavn og password i databasen

---
+++ create.php
@@ -0,0 +1,43 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "valgfag";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$name = $_POST['uname'];
+$psw = $_POST['psw'];
+$repeat = $_POST['psw-repeat'];
+$password = md5($psw);
+
+$check = "SELECT `username` FROM `login`";
+$result = $conn->query($sql);
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    if($row["username"] === $name) {
+      header("Location: /signup.html");
+    }
+  }
+} else {
+  header("Location: /login.html");
+}
+
+$sql = "Insert into `login` (username, password) values ('$name', '$password')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+mysqli_close($conn);
+echo "Connected successfully";
+?>
