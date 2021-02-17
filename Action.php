cd3230b8b623f8c4fcdf420cea48465e23acbbae
mjni.skp <mjni.skp@edu.mercantec.dk>
2021-02-17 13:37:28 +0100

    ændrede navn på klasse

--- Action.php
+++ Action.php
@@ -32,16 +32,16 @@
   <input type="text" placeholder="Search..">
 </div>
 
-<div class="grid-container">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="grid-item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<div class="container">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
+<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
 </div>
 
 </body>
