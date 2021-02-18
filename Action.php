f956649247aabe2bbf0ab92884dbb4591eda1827
Melika Emma Vigh Kjærgaard Pedersen <meli0970@edu.mercantec.dk>
2021-02-18 14:12:12 +0100

    rettelser af tilføj/fjern til min liste

--- Action.php
+++ Action.php
@@ -34,19 +34,31 @@
 <?php
 require('db_connect.php');
 
-$find_videos = mysqli_query($conn, "SELECT * FROM hyper_videoes WHERE movie_type = 'Action'");
+$find_videos = mysqli_query($conn, "SELECT * FROM hyper_videoes");
 while($row = mysqli_fetch_assoc($find_videos)) {
   $id = $row['id'];
   $name = $row['name'];
+  $my_list = $row['my_list'];
   $thumbnail = $row['picture'];
   echo '<div style="text-align:center;display:inline-block">';
   echo '<div style="display:block">';
   echo '<label style="display:block"><b>'.$row["name"].'</b></label>';
-  echo '<input type="checkbox" name="checkbox" id="'.$row["name"].'"/><label>Min liste</label>';
+  echo '<input type="checkbox" name="checkbox" '.($row["my_list"]==1 ? 'checked ' : '').'onclick="myFunction('.$row["id"].')" id="'.$row["id"].'"/><label>Min liste</label>';
   echo '</div>';
   echo '<a href="watch_temp.php?id='.$id.'"><input class="item" type="image" src="'.$thumbnail.'" alt="Submit" width="200" height="300"></a>';
   echo '</div>';
 }
 ?>
-</div>
+  </div>
 </body>
+<script>
+function myFunction(id) {
+  var checkBox = document.getElementById(id);
+  if (checkBox.checked == true) {
+    window.location.href = 'ajax.php?id=' + id + '&back=Action' 
+  } 
+  else {
+    window.location.href = 'ajax.php?id=' + id + '&back=Action' 
+  }
+}
+</script>
--- FortsætMedAtSe.php
+++ FortsætMedAtSe.php
@@ -6,7 +6,7 @@
 <div class="topnav">
   <a href="Streaming.php">Home</a>
   <a href="MinListe.php">Min Liste</a>
-  <a class="active" href="FortsætMedAtSe.php">Fortsæt med at se</a>
+  <a href="FortsætMedAtSe.php">Fortsæt med at se</a>
 
   <div class="dropdown">
     <button class="dropbtn">Film
@@ -15,34 +15,18 @@
     <div class="dropdown-content">
       <a href="Action.php">Action</a>
       <a href="Gyser.php">Gyser</a>
-      <a href="Komedie.php">Komedie</a>
     </div>
   </div> 
   <div class="dropdown">
-    <button class="dropbtn">Serier
+    <button class="dropbtn">Serier 
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="Action.php">Action</a>
-      <a href="Gyser.php">Eventyr</a>
-      <a href="Komedie.php">Komedie</a>
+      <a href="Eventyr.php">Eventyr</a>
+      <a class="active" href="Komedie.php">Komedie</a>
     </div>
   </div> 
 
   <input type="text" placeholder="Search..">
 </div>
-
-<div class="container">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-<input class="item" type="image" src="img/Woods.jpg" alt="Submit" width="200" height="300">
-</div>
-
-
 </body>
