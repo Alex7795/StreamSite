7c916fa683ac40d541ed58247e9f3085e685f128
Simon <simon_hasselholt@yahoo.dk>
2021-02-18 10:20:10 +0100

    fixed thumbail and player

--- upload_hyperlinks.php
+++ upload_hyperlinks.php
@@ -5,9 +5,10 @@ if(isset($_POST['submit'])) {
     $movie_name = $_POST['name'];
     $video = $_POST['hyperlink'];
     $embed_code = substr($video, 32);
+    $thumbnail = $_POST['picture'];
 
     if(!empty($movie_name) && !empty($video)) {
-        mysqli_query($conn, "INSERT INTO hyper_videoes (name, video, embed_code) VALUES ('$movie_name', '$video', '$embed_code')");
+        mysqli_query($conn, "INSERT INTO hyper_videoes (name, video, embed_code, picture) VALUES ('$movie_name', '$video', '$embed_code', '$thumbnail')");
     }
 
     //mysqli_close($conn);
